06211e77109aced625cdf76af0fbc0f0a2437c03
Aljohn19977 <aljohnmirandilla@gmail.com>
2020-02-28 12:31:11 +0800

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,24 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::Class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::Class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::Class);
+    }
+
+    
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,8 +3,22 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo(Question::Class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::Class);
+    }
+
+    public function like()
+    {
+        return $this->hasMany(Like::Class);
+    }
 }
